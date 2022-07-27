5363b1ad163966fa1c7b7afd1383613703370133
emilyerose <emilyrose0021@gmail.com>
2022-07-26 21:39:34 -0400

    added multiple questions, fixed event handler doubling, and added ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ERQuiz
+
+## Description 
+This project attempts to build a simple JavaScript quiz to test our knowledge of the language. 
+(Though, to be fair, the emphasis is on the project, not the quiz itself, which is just a few questions long.) If I were to add to the project, I would add a number of questions and then randomly generate them (without repeating any).
+
+It combines HTML, CSS, and vanilla JavaScript to make a webpage that interactively renders components with event listeners and stores scores to local storage.
+
+## Installation
+Access the game at https://emilyerose.github.io/ERQuiz/. 
+Then play around!
+
+## Usage
+Your score is based on the amount of time left on the clock when you finish the quiz! Other than that, it's pretty straightforward: click on the buttons and see what happens. you can click on 'view high scores' to see the leaderboard!
+
+![screenshot of starter page](./screenshot.png)
+
+## Credits
+thanks to Ben for their help on some issues I was having with this
+
+## License
+none relevant
--- script.js
+++ script.js
@@ -23,18 +23,10 @@ answerSet: ['string','undefined','float','null','boolean'],
 correctIndex: 2}
 ];
 
-var questionObj = {
-    question: 'Which of the following is NOT a JavaScript class?',
-    answerSet: ['string','undefined','float','null','boolean'],
-    correctIndex: 2
-};
+var questionIterator = 0;
 var timeLeft = 0;
 
-document.addEventListener('click', function (event) {
-    if (event.target.matches('.viewScores')){
-        seeHighScores()
-    };
-})
+scoresButton.addEventListener('click', function() {seeHighScores()})
 
 startButton.addEventListener('click', function () {
     startGame();
@@ -44,14 +36,11 @@ function startGame() {
     //first, make the game description and start button go away
     smallText.style.display = 'none';
     startButton.style.display = 'none';
+    //initialize timeleft and question iterator
     timeLeft = 60;
+    questionIterator=0;
     timer.textContent= timeLeft;
     let q = getQ() //where q is the questionObj and getQ grabs that object, in whatever form we choose to do so
-    //if there are no questions left, end the game
-    if (!q) {
-        clearInterval(countdownTimer);
-        endGame(timeLeft);
-    };
     //render the question
     renderQuestion(q);
     //timer
@@ -62,30 +51,43 @@ function startGame() {
         if (timeLeft <= 0) {
             clearInterval(countdownTimer);
             timeLeft = 0;
-          endGame(0); //do all the things you need to do when the game ends. this might not actually end up staying in a funciton
+            endGame(0); //do all the things you need to do when the game ends. this might not actually end up staying in a funciton
         }
       }, 1000);
 
-      //listen for clicks to list items
-      document.addEventListener('click', function (event) {
+      function runGame(event) {
         if (!countdownTimer) {return}; //checks to see whether the game is running. if not, do nothing
         //did we click one of the options? then do something.
-        if (event.target.matches('li')) { 
-            //render the next question if we have any remaining (gotta add an if for that)
-            q=getQ();
-            renderQuestion(q);
+        if (event.target.matches('.listItem')) { 
             var selectedOption = event.target.getAttribute('data-index');
             var answer = document.querySelector('.answer');
             //perform actions based on whether we answered right or wrong
             if (selectedOption != q.correctIndex) {
                 timeLeft=timeLeft-10;
                 answer.textContent ='---Wrong!---'
+                timer.textContent=timeLeft;
             }
             else {
                 answer.textContent = '---Correct!---'
             }
+            //get the next question
+            q=getQ();
+            //if there are no more, end the game with current score
+            if (!q) {
+                clearInterval(countdownTimer); // im REALLY not sure you can do this i dont think countdown timer is even defined here ughhh
+                document.removeEventListener('click', runGame)
+                endGame(timeLeft);
+            }
+            //otherwise, render the next question
+            else {
+                renderQuestion(q);
+            }
         }
-    })
+      }
+
+
+    //listen for clicks to list items
+    document.addEventListener('click', runGame)
 }
 
 function renderQuestion(qObj) {
@@ -110,9 +112,17 @@ function renderQuestion(qObj) {
     document.querySelector('main').lastElementChild.append(answer)
 }
 
+//gets question object
 function getQ() {
-    return questionObj;
+    if (questionIterator<quizArr.length){
+        questionIterator++;
+        return quizArr[questionIterator-1];
+    }
+    else {
+        return null
+    }
 }
+
 
 function endGame(timeRemaining) {
     //get rid of the right/wrong line:
@@ -134,15 +144,17 @@ function endGame(timeRemaining) {
     formEl.appendChild(label);
     label.appendChild(textBox)
     label.appendChild(submitBtn)
-
-    formEl.addEventListener('submit', function (event) {
-        event.preventDefault()
-        var initials = textBox.value; 
-        initials=initials.toUpperCase();
-        //deals with the score
-        addScore(initials,timeRemaining);
-    })
 }
+
+//add event listener on form
+formEl.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var textBox = document.querySelector('input');
+    var initials = textBox.value; 
+    initials=initials.toUpperCase();
+    //deals with the score
+    addScore(initials,timeLeft);
+})
 
 function addScore(initials,score) {
     let scoresToSave=[]; //scores array to be saved
@@ -159,28 +171,32 @@ function addScore(initials,score) {
     }
     //otherwise, if there are scores saved:
     else {
-        for (let i=0; i<savedScores.length;i++) {
-            if (score > savedScores[i]){
-                //add the score and initials to the proper place in their arrays
-                savedScores.splice(i,0,score);
-                scoresToSave=savedScores;
-                savedInitials.splice(i,0,initials);
-                initialsToSave=savedInitials;
-                scoreIndex = i;
-            }
-            break;
+        //if your score is less than or equal to the worst saved score, add it to the end of the scoreboard
+        if (score<=savedScores[savedScores.length-1]){
+            savedScores.push(score);
+            scoresToSave=savedScores;
+            savedInitials.push(initials);
+            initialsToSave= savedInitials;
+            scoreIndex = savedScores.length-1;
         }
-        //if the score is lower than all other scores
-        savedScores.push(score);
-        scoresToSave=savedScores;
-        savedInitials.push(initials);
-        initialsToSave= savedInitials;
-        scoreIndex = savedScores.length-1;
+        //otherwise, see where it belongs and insert it in the scoreboard
+        else{
+            for (let i=0; i<savedScores.length;i++) {
+                if (score > savedScores[i]){
+                    //add the score and initials to the proper place in their arrays
+                    savedScores.splice(i,0,score);
+                    scoresToSave=savedScores;
+                    savedInitials.splice(i,0,initials);
+                    initialsToSave=savedInitials;
+                    scoreIndex = i;
+                    break;
+                }
+            }
+        }
     }
     //save the new scores and initials arrays to local storage
     localStorage.setItem('scores',JSON.stringify(scoresToSave));
     localStorage.setItem('names',JSON.stringify(initialsToSave));
-
     //render highscores page
     seeHighScores(scoreIndex);
 }
@@ -195,15 +211,17 @@ function seeHighScores(index) { //index is an optional parameter here representi
     //get scores and initials from local storage
     let savedScores=JSON.parse(localStorage.getItem('scores'));
     let savedInitials=JSON.parse(localStorage.getItem('names'));
-    //we've added the ol element, now add all the list items (high scores)
-    for (let x=0; x<savedScores.length; x++) {
-        liEl = document.createElement("li");
-        liEl.textContent = savedInitials[x] + ':  ' + savedScores[x];
-        liEl.setAttribute('class','scoreItem');
-        olEl.appendChild(liEl);
-        //do a little styling for your score
-        if (index && x===index) {
-            liEl.style.backgroundColor = 'rgb(132,112,255)';
+    //we've added the ol element, now add all the list items (high scores) as long as they exist
+    if (savedScores) {
+        for (let x=0; x<savedScores.length; x++) {
+            liEl = document.createElement("li");
+            liEl.textContent = savedInitials[x] + ':  ' + savedScores[x];
+            liEl.setAttribute('class','scoreItem');
+            olEl.appendChild(liEl);
+            //do a little styling for your score
+            if (index && x===index) {
+                liEl.style.backgroundColor = 'rgb(132,112,255)';
+            }
         }
     }
     //display play again and clear scores buttons
